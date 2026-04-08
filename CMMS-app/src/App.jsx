e00da04258a0dc8c9e12fbb506118922c39a31ed
Dev Prakash <prakashdev2211@gmail.com>
2026-04-09 02:20:13 +0530

    Commiting after merge

--- CMMS-app/src/App.jsx
+++ CMMS-app/src/App.jsx
@@ -9,75 +9,15 @@ import AdminProtectedRoute from './components/auth/AdminProtectedRoute';
 import ExtrasPage from './pages/ExtrasPage';
 import RebatePage from './pages/RebatePage';
 import BillingPage from './pages/BillingPage'
-<<<<<<< HEAD
-import CartPage from './pages/CartPage';
-import MyBookings from './pages/MyBookings';
-import { CartProvider } from './components/CartPage/CartContext';
-import AdminDashboard from './pages/AdminDashboard';
-import AdminBillingPage from './pages/AdminBillingPage';
-import AdminExtrasManagement from './pages/AdminExtrasManagement';
-import AdminFeedbackPage from './pages/AdminFeedbackPage';
-import AdminRebatePage from './pages/AdminRebatePage';
-import AdminMenuManagement from './pages/AdminMenuManagement';
-import AdminNotificationPage from './pages/AdminNotificationPage';
-import AdminQRScanPage from './pages/AdminQRScanPage';
-=======
 import AdminFeedbackPage from './pages/AdminFeedbackPage';
 import AdminExtrasManagement from './pages/AdminExtrasManagement';
 import AdminBillingPage from './pages/AdminBillingPage';
 import AdminMenuManagement from './pages/AdminMenuManagement';
 import AdminRebatePage from './pages/AdminRebatePage';
->>>>>>> b967ae00ba034adc2b31cfd33ea3f5903164fc47
 
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <CartProvider>
-        <Routes>
-          <Route path="/login" element={<AuthPage />} />
-          
-          {/* Student Protected Routes */}
-          <Route path="/first" element={
-            <ProtectedRoute>
-              <FirstPage />
-            </ProtectedRoute>
-          } />
-          <Route path="/menu" element={
-            <ProtectedRoute>
-              <DailyMenu />
-            </ProtectedRoute>
-          } />
-          <Route path="/feedbacks" element={
-            <ProtectedRoute>
-              <ComplaintPage />
-            </ProtectedRoute>
-          } />
-
-          <Route path='/extras' element={
-            <ProtectedRoute>
-              <ExtrasPage/>
-            </ProtectedRoute>
-          }/>
-
-          <Route path='/rebate' element={
-            <ProtectedRoute>
-              <RebatePage/>
-            </ProtectedRoute>
-          }/>
-
-          <Route path='/billing' element={
-            <ProtectedRoute>
-              <BillingPage/>
-            </ProtectedRoute>
-          }/>
-
-          <Route path='/cart' element={
-            <ProtectedRoute>
-              <CartPage/>
-            </ProtectedRoute>
-          }/>
-=======
       <Routes>
         <Route path="/login" element={<AuthPage />} />
 
@@ -143,7 +83,6 @@ function App() {
         {/* Public Routes */}
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<HomePage />} />
->>>>>>> b967ae00ba034adc2b31cfd33ea3f5903164fc47
 
           <Route path='/my-bookings' element={
             <ProtectedRoute>
